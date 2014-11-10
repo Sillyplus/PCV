9d16ab0a6ff2d3ec3fb3c94213c4118db265da77
Sillyplus <oi_boy@sina.cn>
2014-11-10 13:17:09 +0800

    update

---
+++ CH01/imtools.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os 
+from PIL import Image 
+from numpy import * 
+
+def get_imlist(path):
+    """ Returns a list of filenames for all jpg images in a directory. """
+    
+    return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
+
+def imresize(im, sz):
+    """ Resize an image array using PIL. """
+    
+    pil_im = Image.fromarray(uint8(im))
+
+    return array(pil_im.resize(sz))
+
+def histeq(im, nbr_bins=256):
+    """ Histogram equalization of a grayscale image. """
+
+    # get image histogram
+    imhist, bins = histogram(im.flatten(), nbr_bins, normed = True)
+    cdf = imhist.cumsum()  # cumulative distribution function
+    cdf = 255 * cdf / cdf[-1] # normalize
+
+    # use linear interpolation of cdf to find new pixel values
+    im2 = interp(im.flatten(), bins[:-1], cdf)
+    
+    return im2.reshape(im.shape), cdf
+
+def compute_average(imlist):
+    """ compute the average of of a list of iamges. """
+
+    # open first image and make into array of type float
+    averageim = array(Image.open(imlist[0]), 'f')
+    
+    for imname in imlist[1:]:
+        try:
+            averageim += array(Image.open(imname))
+        except:
+            print imname + '...skipped'
+    averageim /= len(imlist)
+
+    # return averave as uint8
+    return array(averageim, 'uint8')
+
